ffe84e5f0d91e4413da18a12f3fc2248b3ef9ab5
Rohit-Ghorui <142166570+Rohit-Ghorui@users.noreply.github.com>
2025-06-18 21:25:02 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,7 @@ import geminiResponse from "./gemini.js"
 
 const app=express()
 app.use(cors({
-    origin:"https://my-assistant-frontend.onrender.com",
+    origin:"https://my-assistant-frontend1.onrender.com",
     credentials:true
 }))
 const port=process.env.PORT || 5000
